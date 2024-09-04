78ebad3ab50b5713f0b5f7e1f5a79661259786ed
Aman Mehta <amehta20@nyit.edu>
2024-09-04 09:20:13 -0400

    Auto commit on Wed Sep  4 09:20:12 EDT 2024

---
+++ components/util components/small_arrow.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function SmallArrow(props) {
+  let angle = props.angle ? props.angle : "up"; // default value is up
+  return <div className={" arrow-custom-" + angle}></div>;
+}
